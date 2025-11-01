b0b525fd20446ada4283770d6f0d8c41c110ad91
i2hon <ian2honcharuk@gmail.com>
2025-11-01 16:25:54 -0300

    Update TestAcceso.java
    estos métodos llamaran los métodos crate  para cada objeto.

--- BACKEND/src/ar/edu/unrn/seminario/accesos/TestAcceso.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/TestAcceso.java
@@ -10,6 +10,7 @@ public class TestAcceso {
 	public static void main(String[] args) {
 		RolDao rolDao = new RolDAOJDBC();
 		List<Rol> roles = rolDao.findAll();
+		//sfsd
 
 		for (Rol rol : roles) {
 			System.out.println(rol);
@@ -25,6 +26,14 @@ public class TestAcceso {
 //		}
 			
 //		System.out.println(usuarioDao.find("ldifabio"));
+		
 	}
-
+	public static void crearUsuario(UsuarioDao usuario_A,Usuario usuario_B) {
+		usuario_A.create(usuario_B);
+		System.out.println("Usuario creado: " + usuario_B.getNombre());
+	}
+	public static void crearUsuario(RolDao rol_A,Rol rol_B) {
+		rol_A.create(rol_B);
+		System.out.println("Usuario creado: " + rol_B.getNombre());
+	}
 }
