4737679ae21e7f0fd77e7ca62f1e04de22b4ce2c
PedroContrera1 <pedrocontrera1745@gmail.com>
2025-11-06 16:19:27 -0300

    OrdenPedido Validaciones

---
+++ COMMONS/src/ar/edu/unrn/seminario/exception/DataLengthException.java
@@ -0,0 +1,11 @@
+package ar.edu.unrn.seminario.exception;
+
+public class DataLengthException extends Exception{
+	public DataLengthException() {
+		super();
+	}
+
+	public DataLengthException(String message) {
+		super(message);
+	}
+}
