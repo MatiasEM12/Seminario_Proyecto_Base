56f6d074342e2875a9c06a8ece1a4be731b6fec9
i2hon <ian2honcharuk@gmail.com>
2025-11-08 01:25:55 -0300

    Create DataDoubleException.java
    cree esta excepción para los doubles

---
+++ COMMONS/src/ar/edu/unrn/seminario/exception/DataDoubleException.java
@@ -0,0 +1,11 @@
+package ar.edu.unrn.seminario.exception;
+
+public class DataDoubleException extends Exception {
+	public DataDoubleException() {
+		super();
+	}
+
+	public DataDoubleException(String message) {
+		super(message);
+	}
+}
