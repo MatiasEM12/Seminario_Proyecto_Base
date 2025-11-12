5cadd0b78c8d848955d453cbb40ab2116c5b421d
PedroContrera1 <pedrocontrera1745@gmail.com>
2025-11-12 17:54:34 -0300

    AltaOrdenPedido

--- BACKEND/src/ar/edu/unrn/seminario/api/IApi.java
+++ BACKEND/src/ar/edu/unrn/seminario/api/IApi.java
@@ -62,8 +62,11 @@ public interface IApi {
 	
 	public void registrarOrdenRetiro(OrdenRetiro orden) throws DataNullException, DataLengthException, DataDoubleException, StateChangeException;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 	public void registrarOrdenRetiro(OrdenRetiroDTO orden) throws DataNullException, DataLengthException, DataDoubleException, StateChangeException;
 =======
+=======
+>>>>>>> Stashed changes
 	public void registrarOrdenRetiro(OrdenRetiroDTO orden);
 >>>>>>> Stashed changes
 	ArrayList<OrdenRetiroDTO> obtenerOrdenesRetiro();
