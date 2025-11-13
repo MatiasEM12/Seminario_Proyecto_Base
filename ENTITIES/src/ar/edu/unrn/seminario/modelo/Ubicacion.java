4dd64fc5869aea18a72ec429551d96f286a9a785
i2hon <ian2honcharuk@gmail.com>
2025-11-12 23:09:51 -0300

    Update Ubicacion.java

--- ENTITIES/src/ar/edu/unrn/seminario/modelo/Ubicacion.java
+++ ENTITIES/src/ar/edu/unrn/seminario/modelo/Ubicacion.java
@@ -66,6 +66,15 @@ public class Ubicacion {
 		this.coordenada=coordenada;
 		this.codigo=codigo;
 	}
+	public Ubicacion(String codigo, String zona, String barrio, String direccion) {
+		this.codigo=codigo;
+		this.zona=zona;
+		this.barrio=barrio;
+		this.direccion=direccion;
+		
+		}
+	Ubicacion ubicacionPrueba = new Ubicacion("UB007","ZONA-TEST", "BarrioTest", "Calle Falsa 123");
+	
 
 
 
