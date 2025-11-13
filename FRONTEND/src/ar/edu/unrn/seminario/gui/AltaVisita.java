36219fa6047339d3e3ae9b997d43b1c922aa7223
MatiasEM12 <matiasemimellado@gmail.com>
2025-11-12 23:31:50 -0300

    AltaVisita

--- FRONTEND/src/ar/edu/unrn/seminario/gui/AltaVisita.java
+++ FRONTEND/src/ar/edu/unrn/seminario/gui/AltaVisita.java
@@ -20,6 +20,8 @@ import ar.edu.unrn.seminario.dto.VoluntarioDTO;
 
 import javax.swing.JComboBox;
 import javax.swing.JTextArea;
+import javax.swing.JCheckBox;
+import javax.swing.JRadioButton;
 
 public class AltaVisita extends JFrame{
 	/**
@@ -32,12 +34,13 @@ public class AltaVisita extends JFrame{
 	private IApi api;
 	private JTextField txtFecha;
 	private JTextField txtCodigo;
+	private JTextField textField;
 
 	
 	AltaVisita(IApi api) {
 		this.api=api;
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setBounds(100, 100, 700, 450);
+        setBounds(100, 100, 414, 378);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		//setContentPane(contentPane);
@@ -47,29 +50,22 @@ public class AltaVisita extends JFrame{
 		lblNewLabel.setBounds(10, 30, 133, 13);
 		contentPane.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("Voluntario");
-		lblNewLabel_1.setBounds(10, 57, 133, 13);
-		contentPane.add(lblNewLabel_1);
-		
 		JLabel lblNewLabel_1_1 = new JLabel("Tipo:");
-		lblNewLabel_1_1.setBounds(12, 80, 102, 13);
+		lblNewLabel_1_1.setBounds(9, 107, 102, 13);
 		contentPane.add(lblNewLabel_1_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Observaciones:");
-		lblNewLabel_2.setBounds(10, 117, 96, 13);
+		lblNewLabel_2.setBounds(10, 146, 96, 13);
 		contentPane.add(lblNewLabel_2);
 		
 		JButton btnGuardar = new JButton("Guardar");
-		btnGuardar.setBounds(29, 184, 85, 21);
+		btnGuardar.setBounds(161, 307, 85, 21);
 		contentPane.add(btnGuardar);
 		
 		JButton btnNewButton_1 = new JButton("Cerrar");
-		btnNewButton_1.setBounds(232, 184, 85, 21);
+		btnNewButton_1.setBounds(290, 307, 85, 21);
 		contentPane.add(btnNewButton_1);
 		JLabel lblVoluntario = new JLabel("Voluntario asignado:");
-		JComboBox<VoluntarioDTO> comboVoluntarios = new JComboBox<>();
-		comboVoluntarios.setBounds(140, 53, 151, 21);
-		contentPane.add(comboVoluntarios);
 		// Llenado del combo
 		for (VoluntarioDTO v : api.obtenerVoluntarios()) {
 		    comboVoluntarios.addItem(v);
@@ -77,25 +73,13 @@ public class AltaVisita extends JFrame{
 
 		// Agregado a la ventana
 		contentPane.add(lblVoluntario);
-		contentPane.add(comboVoluntarios);
-		
-		JComboBox<String> comboTipo = new JComboBox<>();
-		comboTipo.addItem("INICIAL");
-		comboTipo.addItem("SEGUIMIENTO");
-		comboTipo.addItem("CIERRE");
-		comboTipo.setBounds(140, 77, 151, 19);
-		contentPane.add(comboTipo);
-		
-		JTextArea txtObservaciones = new JTextArea();
-		txtObservaciones.setBounds(140, 117, 151, 53);
-		contentPane.add(txtObservaciones);
 		
 		txtFecha = new JTextField();
 		txtFecha.setBounds(140, 30, 151, 19);
 		contentPane.add(txtFecha);
 		txtFecha.setColumns(10);
 		
-		JLabel lblCodigo = new JLabel("Codigo:");
+		JLabel lblCodigo = new JLabel("Codigo OrdenRetiro:");
 		lblCodigo.setBounds(10, 10, 133, 13);
 		contentPane.add(lblCodigo);
 		
@@ -148,10 +132,36 @@ public class AltaVisita extends JFrame{
 		});
 
 		setContentPane(contentPane);       // montar el panel en el frame
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCancelar.setBounds(26, 307, 85, 21);
+		contentPane.add(btnCancelar);
+		
+		JButton btnSeleccionBien = new JButton("Bienes");
+		btnSeleccionBien.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnSeleccionBien.setBounds(89, 78, 89, 23);
+		contentPane.add(btnSeleccionBien);
+		
+		textField = new JTextField();
+		textField.setBounds(89, 146, 258, 78);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1_1_1 = new JLabel("Seleccioonar ");
+		lblNewLabel_1_1_1.setBounds(9, 83, 102, 13);
+		contentPane.add(lblNewLabel_1_1_1);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Visita Final");
+		rdbtnNewRadioButton.setBounds(26, 264, 109, 23);
+		contentPane.add(rdbtnNewRadioButton);
         setLocationRelativeTo(null);       // centrar
         setVisible(true);  
 	}
-
-
-	
 }
