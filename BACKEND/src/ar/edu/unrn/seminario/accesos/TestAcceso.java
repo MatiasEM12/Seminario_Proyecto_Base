a8b25727a27c9e13cb1a0141900cba284d4764b1
i2hon <ian2honcharuk@gmail.com>
2025-11-13 13:05:15 -0300

    Update TestAcceso.java

--- BACKEND/src/ar/edu/unrn/seminario/accesos/TestAcceso.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/TestAcceso.java
@@ -9,6 +9,7 @@ import ar.edu.unrn.seminario.api.IApi;
 import ar.edu.unrn.seminario.dto.OrdenPedidoDTO;
 import ar.edu.unrn.seminario.dto.OrdenRetiroDTO;
 import ar.edu.unrn.seminario.dto.VisitaDTO;
+import ar.edu.unrn.seminario.exception.StateChangeException;
 import ar.edu.unrn.seminario.modelo.Bien;
 import ar.edu.unrn.seminario.modelo.Donacion;
 import ar.edu.unrn.seminario.modelo.Donante;
@@ -19,8 +20,45 @@ import ar.edu.unrn.seminario.modelo.OrdenRetiro;
 import ar.edu.unrn.seminario.modelo.Visita;
 import ar.edu.unrn.seminario.modelo.Voluntario;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unrn.seminario.modelo.Rol;
+import ar.edu.unrn.seminario.modelo.Usuario;
+import ar.edu.unrn.seminario.api.PersistenceApi;
+import ar.edu.unrn.seminario.api.IApi;
+import ar.edu.unrn.seminario.dto.OrdenPedidoDTO;
+import ar.edu.unrn.seminario.dto.OrdenRetiroDTO;
+import ar.edu.unrn.seminario.dto.VisitaDTO;
+import ar.edu.unrn.seminario.modelo.Bien;
+import ar.edu.unrn.seminario.modelo.Donacion;
+import ar.edu.unrn.seminario.modelo.Donante;
+import ar.edu.unrn.seminario.modelo.Coordenada;
+import ar.edu.unrn.seminario.modelo.Ubicacion;
+import ar.edu.unrn.seminario.modelo.OrdenPedido;
+import ar.edu.unrn.seminario.modelo.OrdenRetiro;
+import ar.edu.unrn.seminario.modelo.Visita;
+import ar.edu.unrn.seminario.modelo.Voluntario;
 public class TestAcceso {
+	public static void main(String[] args) throws StateChangeException {
+		RolDao rolDao = new RolDAOJDBC();
+		List<Rol> roles = rolDao.findAll();
 
+		for (Rol rol : roles) {
+			System.out.println(rol);
+		}
 
+		UsuarioDao usuarioDao = new UsuarioDAOJDBC();
+//		Usuario usuario = new Usuario("ldifabio", "1234", "Lucas", "ldifabio@unrn.edu.ar", new Rol(1, ""));
+//		usuarioDao.create(usuario);
+
+//		List<Usuario> usuarios = usuarioDao.findAll();
+//			for (Usuario u: usuarios) {
+//			System.out.println(u);
+//		}
+
+//		System.out.println(usuarioDao.find("ldifabio"));
+	}
 
 }
