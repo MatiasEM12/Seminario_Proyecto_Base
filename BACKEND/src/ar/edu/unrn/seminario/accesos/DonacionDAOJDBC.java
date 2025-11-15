27ab750cf10fa299e8da5aabc063df3abc3a1c35
i2hon <ian2honcharuk@gmail.com>
2025-11-15 00:40:51 -0300

    Update DonacionDAOJDBC.java
    agregue el  findAllPendinte para que recuperar solo los que sean pendientes

--- BACKEND/src/ar/edu/unrn/seminario/accesos/DonacionDAOJDBC.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/DonacionDAOJDBC.java
@@ -4,17 +4,21 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import ar.edu.unrn.seminario.exception.DataNullException;
+import ar.edu.unrn.seminario.exception.StateChangeException;
 import ar.edu.unrn.seminario.modelo.Bien;
 import ar.edu.unrn.seminario.modelo.Coordenada;
 import ar.edu.unrn.seminario.modelo.Donacion;
 import ar.edu.unrn.seminario.modelo.Donante;
 import ar.edu.unrn.seminario.modelo.OrdenPedido;
+import ar.edu.unrn.seminario.modelo.Rol;
 
 public class DonacionDAOJDBC implements DonacionDAO{
 
@@ -181,10 +185,72 @@ BienDAO  b;
 		return donacion;
 	}
 
+	
 	@Override
-	public List<Donacion> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Donacion> findAll() throws DataNullException{
+		List<Donacion> listado = new ArrayList<>();
+	    String sql = "SELECT d.codigo, d.observacion, d.Fecha_Donacion, d.codigoDonante, d.codigoOrdenPedido FROM donacion d ";
+
+	    try (Connection conn = ConnectionManager.getConnection();
+	         Statement sentencia = conn.createStatement();
+	         ResultSet resultado = sentencia.executeQuery(sql)) {
+
+	        while (resultado.next()) {
+	            String codigo = resultado.getString("codigo");
+	            String observacion = resultado.getString("observacion");
+	            Date Fecha_Donacion = resultado.getDate("Fecha_Donacion");
+	            LocalDate fecha = Fecha_Donacion.toLocalDate();
+	            String codigoDonante = resultado.getString("codigoDonante");
+	            String codigoOrdenPedido = resultado.getString("codigoOrdenPedido");
+	            Donante donante = d.find(codigoDonante);
+	            OrdenPedido pedido = op.find(codigoOrdenPedido);
+	            ArrayList<Bien> bienes = b.findBienDonacion(codigo);
+	            // Usamos el constructor que ya tenés:
+	            Donacion donacion = new Donacion(fecha,observacion,bienes,donante,pedido,codigo);
+	            listado.add(donacion);
+	        }
+	    } catch (SQLException e) {
+			System.out.println("Error de mySql\n" + e.toString());
+			// TODO: disparar Exception propia
+		} finally {
+			ConnectionManager.disconnect();
+		}
+
+	    return listado;
+	}
+	
+	public List<Donacion> findAllPendiente() throws DataNullException {
+		List<Donacion> listado = new ArrayList<>();
+	    String sql = "SELECT d.codigo, d.observacion, d.Fecha_Donacion, d.codigoDonante, d.codigoOrdenPedido "
+	    		+ "FROM donacion d "
+	    		+"JOIN ordenpedido o ON d.codigoOrdenPedido = o.codigo JOIN donante don ON d.codigoDonante = don.codigo "
+	    		+ "WHERE o.estado = 'PENDIENTE'";
+
+	    try (Connection conn = ConnectionManager.getConnection();
+	         Statement sentencia = conn.createStatement();
+	         ResultSet resultado = sentencia.executeQuery(sql)) {
+
+	        while (resultado.next()) {
+	            String codigo = resultado.getString("codigo");
+	            String observacion = resultado.getString("observacion");
+	            Date Fecha_Donacion = resultado.getDate("Fecha_Donacion");
+	            LocalDate fecha = Fecha_Donacion.toLocalDate();
+	            String codigoDonante = resultado.getString("codigoDonante");
+	            String codigoOrdenPedido = resultado.getString("codigoOrdenPedido");
+	            Donante donante = d.find(codigoDonante);
+	            OrdenPedido pedido = op.find(codigoOrdenPedido);
+	            ArrayList<Bien> bienes = b.findBienDonacion(codigo);
+	            // Usamos el constructor que ya tenés:
+	            Donacion donacion = new Donacion(fecha,observacion,bienes,donante,pedido,codigo);
+	            listado.add(donacion);
+	        }
+	    } catch (SQLException e) {
+			System.out.println("Error de mySql\n" + e.toString());
+			// TODO: disparar Exception propia
+		} finally {
+			ConnectionManager.disconnect();
+		}
+	    return listado;
 	}
 
 }
