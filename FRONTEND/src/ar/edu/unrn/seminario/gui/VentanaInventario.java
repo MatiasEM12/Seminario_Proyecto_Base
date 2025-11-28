1cfc36d575242c1684abada0e2204cbd15aa30f9
i2hon <ian2honcharuk@gmail.com>
2025-11-28 16:43:49 -0300

    Create VentanaInventario.java
    ventana inventario

---
+++ FRONTEND/src/ar/edu/unrn/seminario/gui/VentanaInventario.java
@@ -0,0 +1,43 @@
+package ar.edu.unrn.seminario.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class VentanaInventario {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaInventario window = new VentanaInventario();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VentanaInventario() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
