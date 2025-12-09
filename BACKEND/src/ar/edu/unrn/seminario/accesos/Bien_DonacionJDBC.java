4b32f3c6af9fb089223172cdf3b2e3c39b49eb52
i2hon <ian2honcharuk@gmail.com>
2025-12-09 00:51:17 -0300

    Update Bien_DonacionJDBC.java

--- BACKEND/src/ar/edu/unrn/seminario/accesos/Bien_DonacionJDBC.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/Bien_DonacionJDBC.java
@@ -29,11 +29,11 @@ public class Bien_DonacionJDBC implements Bien_DonacionDAO {
 
             int cantidad = statement.executeUpdate();
             if (cantidad <= 0) {
-                System.out.println("No se insertó registro en Bien_Donacion codigo error B100");
+                System.out.println("No se insertó registro en Bien_Donacion codigo error BD100");
             }
 
         } catch (SQLException e) {
-            System.out.println("Error al procesar consulta (INSERT Bien_Donacion): " + e.getMessage() + "codigo error B101");
+            System.out.println("Error al procesar consulta (INSERT Bien_Donacion): " + e.getMessage() + "codigo error BD101");
             e.printStackTrace();
             throw new RuntimeException(e); 
         } finally {
@@ -58,11 +58,11 @@ public class Bien_DonacionJDBC implements Bien_DonacionDAO {
             if (cantidad > 0) {
                 System.out.println("El Bien_Donacion se ha actualizado correctamente");
             } else {
-                System.out.println("No se encontró registro a actualizar en Bien_Donacion codigo error B200");
+                System.out.println("No se encontró registro a actualizar en Bien_Donacion codigo error BD200");
             }
 
         } catch (SQLException e) {
-            System.out.println("Error al procesar consulta (UPDATE Bien_Donacion): " + e.getMessage() + "codigo error B201" );
+            System.out.println("Error al procesar consulta (UPDATE Bien_Donacion): " + e.getMessage() + "codigo error BD201" );
             e.printStackTrace();
         } finally {
             ConnectionManager.disconnect();
@@ -126,7 +126,7 @@ public class Bien_DonacionJDBC implements Bien_DonacionDAO {
             }
 
         } catch (SQLException e) {
-            System.out.println("Error al recuperar bienes de donación: " + e.getMessage() + "codigo error B300");
+            System.out.println("Error al recuperar bienes de donación: " + e.getMessage() + "codigo error BD300");
             e.printStackTrace();
         } finally {
             try { if (rs != null) rs.close(); } catch (SQLException ignored) {}
