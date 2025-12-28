5c017bef572569ef0d838bb40544a69fdd7295a9
i2hon <ian2honcharuk@gmail.com>
2025-12-28 04:41:22 -0300

    Create InventarioDAO.java
    inventario dao

---
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/InventarioDAO.java
@@ -0,0 +1,29 @@
+package ar.edu.unrn.seminario.accesos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unrn.seminario.modelo.Bien;
+
+
+public interface InventarioDAO {
+	void create(Bien bien);
+
+	void update(Bien bien);
+
+	void remove(Long id);
+	
+	void remove(String codigo);
+
+	void remove(Bien bien);
+
+	Bien find(String codigo);
+
+	List<Bien> findAll();
+
+	ArrayList<Bien> findBienDonacion(String codDonacion);
+
+	ArrayList<Bien> findBienVisita(String codVisita);
+	
+	List<Bien> findALLTipo(String tipo);
+}
