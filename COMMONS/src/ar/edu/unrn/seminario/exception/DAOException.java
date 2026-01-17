16167947409ba2ca63b3c7da6dc4ca34e9ecb517
i2hon <ian2honcharuk@gmail.com>
2026-01-16 21:48:11 -0300

    Create DAOException.java
    la excepción para los dao hay que actualizar todos los dao con esta nueva excepción

---
+++ COMMONS/src/ar/edu/unrn/seminario/exception/DAOException.java
@@ -0,0 +1,11 @@
+package ar.edu.unrn.seminario.exception;
+
+public class DAOException extends Exception{
+	public DAOException() {
+		super();
+	}
+
+	public DAOException(String message) {
+		super(message);
+	}
+}
