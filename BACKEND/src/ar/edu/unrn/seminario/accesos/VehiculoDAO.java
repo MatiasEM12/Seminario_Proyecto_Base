aab1de7ea363952d042d22c6c7ae268ac5084403
i2hon <ian2honcharuk@gmail.com>
2026-01-16 21:47:12 -0300

    Create VehiculoDAO.java
    vehiculo dao

---
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/VehiculoDAO.java
@@ -0,0 +1,20 @@
+package ar.edu.unrn.seminario.accesos;
+
+import java.util.List;
+import ar.edu.unrn.seminario.exception.*;
+
+
+import ar.edu.unrn.seminario.modelo.Vehiculo;
+
+public interface VehiculoDAO {
+
+	void create(Vehiculo vehiculo) throws DAOException;
+
+	void update(Vehiculo vehiculo) throws DAOException;
+	
+	void remove(String matricula) throws DAOException;
+
+	Vehiculo find(String matricula);
+
+	List<Vehiculo> findAll();
+}
