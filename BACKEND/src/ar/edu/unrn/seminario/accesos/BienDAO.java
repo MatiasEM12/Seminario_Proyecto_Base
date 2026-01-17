3e4d89d25027369f7a56ee5f8c9afced69fe9399
i2hon <ian2honcharuk@gmail.com>
2026-01-17 20:41:43 -0300

    Update BienDAO.java

--- BACKEND/src/ar/edu/unrn/seminario/accesos/BienDAO.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/BienDAO.java
@@ -3,27 +3,29 @@ package ar.edu.unrn.seminario.accesos;
 import java.util.ArrayList;
 import java.util.List;
 
+import ar.edu.unrn.seminario.exception.DAOException;
+import ar.edu.unrn.seminario.exception.DataNullException;
 import ar.edu.unrn.seminario.modelo.Bien;
 
 public interface BienDAO {
 
-	void create(Bien bien);
+	void create(Bien bien) throws DAOException;
 
-	void update(Bien bien);
+	void update(Bien bien) throws DAOException;
 
 	void remove(Long id);
 	
-	void remove(String codigo);
+	void remove(String codigo)throws DAOException;
 
-	void remove(Bien bien);
+	void remove(Bien bien)throws DAOException;
 
-	Bien find(String codigo);
+	Bien find(String codigo)throws DataNullException, DAOException;
 
-	List<Bien> findAll();
+	List<Bien> findAll()throws DAOException;
 
-	ArrayList<Bien> findBienDonacion(String codDonacion);
+	ArrayList<Bien> findBienDonacion(String codDonacion)throws DataNullException,DAOException;
 
-	ArrayList<Bien> findBienVisita(String codVisita);
+	ArrayList<Bien> findBienVisita(String codVisita)throws DataNullException,DAOException;
 	
-	List<Bien> findALLTipo(String tipo);
+	List<Bien> findALLTipo(String tipo)throws DataNullException,DAOException;
 }
