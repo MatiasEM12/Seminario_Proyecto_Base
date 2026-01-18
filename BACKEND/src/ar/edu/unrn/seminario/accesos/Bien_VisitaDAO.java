2ecf3377cf621dc9726aa51419684a0652829e1e
i2hon <ian2honcharuk@gmail.com>
2026-01-17 21:01:35 -0300

    Update Bien_VisitaDAO.java

--- BACKEND/src/ar/edu/unrn/seminario/accesos/Bien_VisitaDAO.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/Bien_VisitaDAO.java
@@ -2,25 +2,26 @@ package ar.edu.unrn.seminario.accesos;
 
 import java.util.List;
 
+import ar.edu.unrn.seminario.exception.DAOException;
 import ar.edu.unrn.seminario.modelo.Bien;
 
 public interface Bien_VisitaDAO {
 
 	
 
-	void create(String codBien, String codVisita);
+	void create(String codBien, String codVisita) throws DAOException;
 
 
 
 	void remove(Long id);
 	
-	void remove(String codigoVisita);
+	void remove(String codigoVisita) ;
 
 
-	List<Bien> findAll();
+	List<Bien> findAll() ;
 
-	List<Bien> findVisita(String codVisita);
+	List<Bien> findVisita(String codVisita)throws DAOException;
 
-	void update(String codBienNuevo, String codBienViejo, String codVisita);
+	void update(String codBienNuevo, String codBienViejo, String codVisita) throws DAOException;
 
 }
