aef1f8939bc1977ef95633e4d7f779041f8ae2ed
i2hon <ian2honcharuk@gmail.com>
2026-01-17 23:25:13 -0300

    Update UbicacionDAO.java

--- BACKEND/src/ar/edu/unrn/seminario/accesos/UbicacionDAO.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/UbicacionDAO.java
@@ -2,23 +2,25 @@ package ar.edu.unrn.seminario.accesos;
 
 import java.util.List;
 
+import ar.edu.unrn.seminario.exception.DAOException;
+import ar.edu.unrn.seminario.exception.DataNullException;
 import ar.edu.unrn.seminario.modelo.Ubicacion;
 
 
 
 public interface UbicacionDAO {
 
-	void create(Ubicacion ubicacion);
+	void create(Ubicacion ubicacion) throws DAOException;
 
-	void update(Ubicacion ubicacion);
+	void update(Ubicacion ubicacion)throws DAOException;
 
 	void remove(Long id);
 	
-	void remove(String codigo);
+	void remove(String codigo)throws DAOException, DataNullException;
 
-	void remove(Ubicacion ubicacion);
+	void remove(Ubicacion ubicacion)throws DAOException, DataNullException;
 
-	Ubicacion find(String codigo);
+	Ubicacion find(String codigo)throws DAOException;
 
-	List<Ubicacion> findAll();
+	List<Ubicacion> findAll()throws DAOException;
 }
