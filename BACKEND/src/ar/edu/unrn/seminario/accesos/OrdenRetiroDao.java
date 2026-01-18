afada41e4bf8a885dab165d8d98b4800c08bdec0
i2hon <ian2honcharuk@gmail.com>
2026-01-17 22:55:55 -0300

    Update OrdenRetiroDao.java

--- BACKEND/src/ar/edu/unrn/seminario/accesos/OrdenRetiroDao.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/OrdenRetiroDao.java
@@ -1,18 +1,20 @@
 package ar.edu.unrn.seminario.accesos;
 
 import java.util.List;
+
+import ar.edu.unrn.seminario.exception.DAOException;
 import ar.edu.unrn.seminario.modelo.OrdenRetiro;
 
 public interface OrdenRetiroDao {
-	void create(OrdenRetiro orden);
+	void create(OrdenRetiro orden)throws DAOException;
 
-	void update(OrdenRetiro orden);
+	void update(OrdenRetiro orden)throws DAOException;
 
-	void remove(String codigo);
+	void remove(String codigo)throws DAOException;
 
-	void remove(OrdenRetiro orden);
+	void remove(OrdenRetiro orden)throws DAOException;
 
-	OrdenRetiro find(String codigo);
+	OrdenRetiro find(String codigo)throws DAOException;
 
-	List<OrdenRetiro> findAll();
+	List<OrdenRetiro> findAll()throws DAOException;
 }
