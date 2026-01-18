4d7d39fe45da82744c7a6ce87a6c40563005c9f0
i2hon <ian2honcharuk@gmail.com>
2026-01-17 23:46:37 -0300

    Update UsuarioDao.java

--- BACKEND/src/ar/edu/unrn/seminario/accesos/UsuarioDao.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/UsuarioDao.java
@@ -3,20 +3,21 @@ package ar.edu.unrn.seminario.accesos;
 import java.sql.SQLException;
 import java.util.List;
 
+import ar.edu.unrn.seminario.exception.DAOException;
 import ar.edu.unrn.seminario.exception.DataNullException;
 import ar.edu.unrn.seminario.modelo.Usuario;
 
 public interface UsuarioDao {
-	void create(Usuario Usuario);
+	void create(Usuario Usuario)throws DAOException;
 
-	void update(Usuario Usuario);
+	void update(Usuario Usuario)throws DAOException;
 
 	void remove(Long id);
 
-	void remove(Usuario Usuario);
+	void remove(Usuario Usuario)throws DAOException;
 
-	Usuario find(String username);
+	Usuario find(String username)throws DAOException;
 
-	List<Usuario> findAll() throws DataNullException;
+	List<Usuario> findAll() throws DataNullException, DAOException;
    int obtenerCantidadUsuarios() throws SQLException;
 }
