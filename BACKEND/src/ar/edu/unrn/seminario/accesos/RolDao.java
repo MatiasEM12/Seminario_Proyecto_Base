c64d1cc375a46fc2b8737debb106952d50a1ec5b
i2hon <ian2honcharuk@gmail.com>
2026-01-17 23:13:03 -0300

    Update RolDao.java

--- BACKEND/src/ar/edu/unrn/seminario/accesos/RolDao.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/RolDao.java
@@ -2,21 +2,23 @@ package ar.edu.unrn.seminario.accesos;
 
 import java.util.List;
 
+import ar.edu.unrn.seminario.exception.DAOException;
+import ar.edu.unrn.seminario.exception.DataNullException;
 import ar.edu.unrn.seminario.exception.StateChangeException;
 import ar.edu.unrn.seminario.modelo.Rol;
 
 public interface RolDao {
-	void create(Rol rol);
+	void create(Rol rol)throws DAOException;
 
-	void update(Rol rol);
+	void update(Rol rol)throws DAOException;
 
-	void remove(Integer id);
+	void remove(Integer codigo)throws DAOException;
 
-	void remove(Rol rol);
+	void remove(Rol rol)throws DAOException;
 
-	Rol find(Integer codigo);
+	Rol find(Integer codigo)throws DAOException;
 
-	List<Rol> findAll() throws StateChangeException;
+	List<Rol> findAll() throws DataNullException, DAOException;
 	
 	
 
