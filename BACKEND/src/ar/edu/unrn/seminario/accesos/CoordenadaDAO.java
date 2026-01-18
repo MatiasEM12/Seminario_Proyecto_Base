ea889fff28b0904e0ebe3ebe8b38cf549e57ea9e
i2hon <ian2honcharuk@gmail.com>
2026-01-17 21:01:37 -0300

    Update CoordenadaDAO.java

--- BACKEND/src/ar/edu/unrn/seminario/accesos/CoordenadaDAO.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/CoordenadaDAO.java
@@ -2,22 +2,24 @@ package ar.edu.unrn.seminario.accesos;
 
 import java.util.List;
 
+import ar.edu.unrn.seminario.exception.DAOException;
+import ar.edu.unrn.seminario.exception.DataNullException;
 import ar.edu.unrn.seminario.modelo.Coordenada;
 
 
 public interface CoordenadaDAO {
-	void create(Coordenada coordenada);
+	void create(Coordenada coordenada)throws DAOException;
 
-	void update(Coordenada coordenada);
+	void update(Coordenada coordenada)throws DAOException;
 
 	void remove(Long id);
 	
-	void remove(String codigo);
+	void remove(String codigo)throws DataNullException, DAOException;
 
-	void remove(Coordenada coordenada);
+	void remove(Coordenada coordenada)throws DAOException;
 
-	Coordenada find(String codigo);
+	Coordenada find(String codigo)throws DataNullException,DAOException;
 
-	List<Coordenada> findAll();
+	List<Coordenada> findAll()throws DAOException;
 
 }
