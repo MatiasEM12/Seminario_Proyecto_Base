d2b4dff0e260d2d77813b31c46d2a6eb595accd3
i2hon <ian2honcharuk@gmail.com>
2026-01-17 22:48:02 -0300

    Update OrdenPedidoDao.java

--- BACKEND/src/ar/edu/unrn/seminario/accesos/OrdenPedidoDao.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/OrdenPedidoDao.java
@@ -2,19 +2,20 @@ package ar.edu.unrn.seminario.accesos;
 
 import java.util.List;
 
+import ar.edu.unrn.seminario.exception.DAOException;
 import ar.edu.unrn.seminario.modelo.OrdenPedido;
 
 public interface OrdenPedidoDao {
-	void create(OrdenPedido Usuario);
+	void create(OrdenPedido Usuario)throws DAOException;
 
-	void update(OrdenPedido Usuario);
+	void update(OrdenPedido Usuario)throws DAOException;
 
-	void remove(String id);
+	void remove(String id)throws DAOException;
 
-	void remove(OrdenPedido Usuario);
+	void remove(OrdenPedido Usuario)throws DAOException;
 
-	OrdenPedido find(String username);
+	OrdenPedido find(String username)throws DAOException;
 
-	List<OrdenPedido> findAll();
+	List<OrdenPedido> findAll() throws DAOException;
 
 }
