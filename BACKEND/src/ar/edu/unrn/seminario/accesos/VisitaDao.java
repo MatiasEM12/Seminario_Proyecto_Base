da8bace877216cdbaef00461901bb92dfa16abb0
i2hon <ian2honcharuk@gmail.com>
2026-01-17 23:57:32 -0300

    Update VisitaDao.java

--- BACKEND/src/ar/edu/unrn/seminario/accesos/VisitaDao.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/VisitaDao.java
@@ -3,22 +3,23 @@ package ar.edu.unrn.seminario.accesos;
 import java.util.ArrayList;
 import java.util.List;
 
+import ar.edu.unrn.seminario.exception.DAOException;
 import ar.edu.unrn.seminario.exception.DataLengthException;
 import ar.edu.unrn.seminario.exception.DataNullException;
 import ar.edu.unrn.seminario.modelo.Visita;
 
 public interface VisitaDao {
-	void create(Visita visita);
+	void create(Visita visita)throws DAOException;
 
-	void update(Visita visita);
+	void update(Visita visita)throws DAOException;
 
-	void remove(String id);
+	void remove(String codigo)throws DAOException;
 
-	void remove(Visita visita );
+	void remove(Visita visita )throws DAOException;
 
-	Visita find(String username) throws DataNullException, DataLengthException;
+	Visita find(String username) throws DataNullException, DataLengthException, DAOException;
 
-	List<Visita> findAll() throws DataNullException, DataLengthException;
+	List<Visita> findAll() throws DataNullException, DataLengthException, DAOException;
 
-	ArrayList<Visita> findAll(String codOrdenRetiro) throws DataNullException, DataLengthException;
+	ArrayList<Visita> findAll(String codOrdenRetiro) throws DataNullException, DataLengthException, DAOException;
 }
