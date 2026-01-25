604892002e5cdbdf2c9d75f8c1c64bdcc788dd2f
i2hon <ian2honcharuk@gmail.com>
2026-01-25 15:59:11 -0300

    Update InventarioDAOJDBC.java
    implemente los throws y el cambiarEntrega

--- BACKEND/src/ar/edu/unrn/seminario/accesos/InventarioDAOJDBC.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/InventarioDAOJDBC.java
@@ -8,19 +8,21 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import ar.edu.unrn.seminario.exception.DAOException;
 import ar.edu.unrn.seminario.modelo.Bien;
 
 public class InventarioDAOJDBC implements InventarioDAO{
 
 	
-public void create(Bien bien) {
+public void create(Bien bien) throws DAOException {
 		
 		try {
 			
 			Connection conn = ConnectionManager.getConnection();
 			PreparedStatement statement = conn
+					// tienen otro atributo llamado entregado para saber si el bien fue entregado. 0 = no entregao, 1 = entregado, esta en default como 0.
 					.prepareStatement("INSERT INTO inventario(codigo,tipo,nombre,peso,descripcion,nivelNecesidad,fechaVencimiento,talle,material)"
-							+ " VALUES (?, ?, ?,?,?,?,?,?,?)");
+							+ " VALUES (?, ?, ?,?,?,?,?,?,?,?)");
 			
 			java.sql.Date fechaSQL = java.sql.Date.valueOf(bien.getFechaVencimiento());
 			statement.setString(1, bien.getCodigo());
@@ -37,12 +39,12 @@ public void create(Bien bien) {
 			if (cantidad > 0) {
 				// System.out.println("Modificando " + cantidad + " registros");
 			} else {
-				System.out.println("Error al actualizar. codigo error I100");
+				throw new DAOException("Error al actualizar. codigo error I100");
 				// TODO: disparar Exception propia
 			}
 
 		} catch (SQLException e) {
-	        System.out.println("Error al procesar consulta (INSERT Bien): " + e.getMessage() + ". codigo error I101");
+			throw new DAOException("Error al procesar consulta (INSERT Bien): " + e.getMessage() + ". codigo error I101");
 	    } finally {
 	        ConnectionManager.disconnect();
 	    }
@@ -50,7 +52,7 @@ public void create(Bien bien) {
 	}
 
 	@Override
-	public void update(Bien bien) {
+	public void update(Bien bien) throws DAOException {
 		try {
 
 			Connection conn = ConnectionManager.getConnection();
@@ -71,18 +73,45 @@ public void create(Bien bien) {
 			if (cantidad > 0) {
 				 System.out.println("El inventario se ha actualizado correctamente");
 			} else {
-				System.out.println("Error al actualizar. codigo error I200");
+				throw new DAOException("Error al actualizar. codigo error I200");
 				// TODO: disparar Exception propia
 			}
 
 		} catch (SQLException e) {
-			System.out.println("Error al procesar consulta. codigo error I201");
+			throw new DAOException("Error al procesar consulta."+ e.getMessage() +" codigo error I201");
 			// TODO: disparar Exception propia
 		} finally {
 			ConnectionManager.disconnect();
 		}
 		
 	}
+	public void cambiarEntrega(String codigo) throws DAOException {
+		try {
+
+			Connection conn = ConnectionManager.getConnection();
+			//el NOT estregado ase que de 0->1,1->0 del bien que coincida el codigo.
+			PreparedStatement statement = conn
+					.prepareStatement("UPDATE inventario SET entregado=NOT entregado WHERE codigo = ?");
+			
+			statement.setString(1, codigo);
+			
+			int cantidad = statement.executeUpdate();
+			if (cantidad > 0) {
+				 System.out.println("El estado se ha actualizado correctamente");
+			} else {
+				throw new DAOException("Error no se encontro el bien. codigo error i250");
+				// TODO: disparar Exception propia
+			}
+
+		} catch (SQLException e) {
+			throw new DAOException("Error al procesar consulta."+ e.getMessage() +" codigo error I251");
+			// TODO: disparar Exception propia
+		} finally {
+			ConnectionManager.disconnect();
+		}
+		
+	}
+	
 
 	@Override
 	public void remove(Long id) {
@@ -91,7 +120,7 @@ public void create(Bien bien) {
 	}
 
 	@Override
-	public void remove(String codigo) {
+	public void remove(String codigo) throws DAOException {
 		try {
 			 Connection conn = ConnectionManager.getConnection();
 		        PreparedStatement statement = conn.prepareStatement(
@@ -104,11 +133,11 @@ public void create(Bien bien) {
 		        if (cantidad > 0) {
 		            System.out.println("inventario eliminado correctamente el bien.");
 		        } else {
-		            System.out.println("No se encontró el bien en el inventario. codigo error I300");
+		        	throw new DAOException("No se encontró el bien en el inventario. codigo error I300");
 		        }
 			
 		}catch(SQLException e) {
-			System.out.println("Error al Eliminar bien del inventario. codigo error I301");
+			throw new DAOException("Error al Eliminar bien del inventario."+ e.getMessage() +" codigo error I301");
 		}finally {
 			ConnectionManager.disconnect();
 		}
@@ -116,7 +145,7 @@ public void create(Bien bien) {
 	}
 
 	@Override
-	public void remove(Bien bien) {
+	public void remove(Bien bien) throws DAOException {
 		try {
 			 Connection conn = ConnectionManager.getConnection();
 		        PreparedStatement statement = conn.prepareStatement(
@@ -129,11 +158,11 @@ public void create(Bien bien) {
 		        if (cantidad > 0) {
 		            System.out.println("bien eliminado correctamente.");
 		        } else {
-		            System.out.println("No se encontró el bien en el inventario. codigo error I400");
+		        	throw new DAOException("No se encontró el bien en el inventario. codigo error I400");
 		        }
 			
 		}catch(SQLException e) {
-			System.out.println("Error al Eliminar bien del inventario. codigo error I401");
+			throw new DAOException("Error al Eliminar bien del inventario."+ e.getMessage() +" codigo error I401");
 		}finally {
 			ConnectionManager.disconnect();
 		}
@@ -142,7 +171,7 @@ public void create(Bien bien) {
 	}
 
 	@Override
-	public Bien find(String codigo) {
+	public Bien find(String codigo) throws DAOException {
 		Bien bien= null;
 		try {
 			Connection conn= ConnectionManager.getConnection();
@@ -163,10 +192,10 @@ public void create(Bien bien) {
 			}
 		}
 		catch(SQLException e){
-			System.out.println("Error al procesar consulta"+ e.getMessage()+". codigo error I500");
+			throw new DAOException("Error al procesar consulta"+ e.getMessage()+". codigo error I500");
 		}
 		catch (Exception e) {
-			System.out.println("Error inesperado: " + e.getMessage()+". codigo error I501");
+			throw new DAOException("Error inesperado: " + e.getMessage()+". codigo error I501");
 		} 
 		finally {
 			ConnectionManager.disconnect();
@@ -175,7 +204,7 @@ public void create(Bien bien) {
 	}
 
 	@Override
-	public List<Bien> findAll() {
+	public List<Bien> findAll() throws DAOException {
 List<Bien> bienes = new ArrayList<>();
 		
 		try {
@@ -189,10 +218,10 @@ List<Bien> bienes = new ArrayList<>();
 			}
 		}
 		catch(SQLException e){
-			System.out.println("Error al procesar consulta"+ e.getMessage()+". codigo error I600");
+			throw new DAOException("Error al procesar consulta"+ e.getMessage()+". codigo error I600");
 		}
 		catch (Exception e) {
-			System.out.println("Error inesperado: " + e.getMessage()+". codigo error I601");
+			throw new DAOException("Error inesperado: " + e.getMessage()+". codigo error I601");
 		} 
 		finally {
 			ConnectionManager.disconnect();
@@ -200,7 +229,7 @@ List<Bien> bienes = new ArrayList<>();
 		return bienes;
 	}
 	@Override
-	public ArrayList<Bien> findBienVisita(String codVisita) {
+	public ArrayList<Bien> findBienVisita(String codVisita) throws DAOException {
 ArrayList<Bien> bienes = new ArrayList<>();
 		
 		try {
@@ -217,10 +246,10 @@ ArrayList<Bien> bienes = new ArrayList<>();
 			}
 		}
 		catch(SQLException e){
-			System.out.println("Error al procesar consulta"+ e.getMessage()+". codigo error I700");
+			throw new DAOException("Error al procesar consulta"+ e.getMessage()+". codigo error I700");
 		}
 		catch (Exception e) {
-			System.out.println("Error inesperado: " + e.getMessage()+". codigo error I701");
+			throw new DAOException("Error inesperado: " + e.getMessage()+". codigo error I701");
 		} 
 		finally {
 			ConnectionManager.disconnect();
@@ -229,7 +258,7 @@ ArrayList<Bien> bienes = new ArrayList<>();
 	}
 	
 	@Override
-	public ArrayList<Bien> findBienDonacion(String codDonacion) {
+	public ArrayList<Bien> findBienDonacion(String codDonacion) throws DAOException {
 		ArrayList<Bien> bienes = new ArrayList<>();
 		
 		try {
@@ -246,10 +275,10 @@ ArrayList<Bien> bienes = new ArrayList<>();
 			}
 		}
 		catch(SQLException e){
-			System.out.println("Error al procesar consulta"+ e.getMessage()+". codigo error I801");
+			throw new DAOException("Error al procesar consulta"+ e.getMessage()+". codigo error I801");
 		}
 		catch (Exception e) {
-			System.out.println("Error inesperado: " + e.getMessage()+". codigo error I801");
+			throw new DAOException("Error inesperado: " + e.getMessage()+". codigo error I801");
 		} 
 		finally {
 			ConnectionManager.disconnect();
@@ -263,7 +292,7 @@ ArrayList<Bien> bienes = new ArrayList<>();
 	// para iventario
 	
 	
-	public List<Bien> findALLTipo(String tipo) {
+	public List<Bien> findALLTipo(String tipo) throws DAOException {
 	    List<Bien> bienes = new ArrayList<>();
 	    try {
 	        //rebisa si la busqueda no fue por bienes vencidos
@@ -316,9 +345,9 @@ ArrayList<Bien> bienes = new ArrayList<>();
 	            }
 	        }
 	    } catch (SQLException e) {
-	        System.out.println("Error al procesar consulta: " + e.getMessage()+". codigo error I900");
+	    	throw new DAOException("Error al procesar consulta: " + e.getMessage()+". codigo error I900");
 	    } catch (Exception e) {
-	        System.out.println("Error inesperado: " + e.getMessage()+". codigo error I901");
+	    	throw new DAOException("Error inesperado: " + e.getMessage()+". codigo error I901");
 	    } finally {
 	        ConnectionManager.disconnect();
 	    }
