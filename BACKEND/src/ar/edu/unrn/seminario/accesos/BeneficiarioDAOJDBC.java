9906d799ec3faefd454735740c9e6cfaf5211e91
PedroContrera1 <pedrocontrera1745@gmail.com>
2026-01-26 20:15:30 -0300

    .

--- BACKEND/src/ar/edu/unrn/seminario/accesos/BeneficiarioDAOJDBC.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/BeneficiarioDAOJDBC.java
@@ -1,5 +1,93 @@
 package ar.edu.unrn.seminario.accesos;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class BeneficiarioDAOJDBC {
 
+import ar.edu.unrn.seminario.exception.DAOException;
+import ar.edu.unrn.seminario.exception.DataNullException;
+import ar.edu.unrn.seminario.modelo.Beneficiario;
+
+public class BeneficiarioDAOJDBC implements BeneficiarioDAO{
+	@Override
+	public void create(Beneficiario beneficiario) throws DAOException{
+		
+		try {
+			
+			Connection conn = ConnectionManager.getConnection();
+			PreparedStatement statement = conn
+					.prepareStatement("INSERT INTO coordenada(codigo,Latitud,Longitud)"
+							+ " VALUES (?, ?, ?)");
+			
+			statement.setString(1, beneficiario.getCodigo());
+			statement.setDouble(2, coordenada.getLatitud());
+			statement.setDouble(3, coordenada.getLongitud());
+		
+			int cantidad = statement.executeUpdate();
+			if (cantidad > 0) {
+				// System.out.println("Modificando " + cantidad + " registros");
+			} else {
+				throw new DAOException("Error al actualizar. codigo error C100");
+				// TODO: disparar Exception propia
+			}
+
+		} catch (SQLException e) {
+			throw new DAOException("Error al procesar consulta. codigo error C101");
+			// TODO: disparar Exception propia
+		} finally {
+			ConnectionManager.disconnect();
+		}
+	
+	}
+
+	@Override
+	public void update(Beneficiario beneficiario) throws DAOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(String codigo) throws DAOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(Beneficiario beneficiario) throws DAOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Beneficiario find(String codigo) throws DataNullException, DAOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Beneficiario> findAll() throws DAOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Beneficiario> findBienDonacion(String codDonacion) throws DataNullException, DAOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Beneficiario> findBienVisita(String codVisita) throws DataNullException, DAOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Beneficiario> findALLTipo(String tipo) throws DataNullException, DAOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- ENTITIES/src/ar/edu/unrn/seminario/modelo/OrdenEntrega.java
+++ ENTITIES/src/ar/edu/unrn/seminario/modelo/OrdenEntrega.java
@@ -12,9 +12,9 @@ public class OrdenEntrega extends Orden{
 	public static String tipo="ORDEN_ENTREGA";
 	private String codigo=null;
 	private LocalDateTime fechaHoraProgramada=null;
-	// comiensa siempre como pendiente que es true
+	// comienza siempre como pendiente que es true
 	private boolean entregaEstado = true;
-	//esto podriamos usuarlo para saver si ya esta definida para una ruta o usar directamente la fecha, si es null es que no esta en marcha
+	//esto podriamos usuarlo para saber si ya esta definida para una ruta o usar directamente la fecha, si es null es que no esta en marcha
 	private boolean confimacionRecepcion=false;
 	private Bien Entrega[];
 	private Beneficiario beneficiario;
