dd55f4d463ef963816fb2c400c96bb3870b24e8e
i2hon <ian2honcharuk@gmail.com>
2026-01-27 20:36:35 -0300

    Update UsuarioDao.java

--- BACKEND/src/ar/edu/unrn/seminario/accesos/UsuarioDao.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/UsuarioDao.java
@@ -4,11 +4,12 @@ import java.sql.SQLException;
 import java.util.List;
 
 import ar.edu.unrn.seminario.exception.DAOException;
+import ar.edu.unrn.seminario.exception.DataExistsException;
 import ar.edu.unrn.seminario.exception.DataNullException;
 import ar.edu.unrn.seminario.modelo.Usuario;
 
 public interface UsuarioDao {
-	void create(Usuario Usuario)throws DAOException;
+	void create(Usuario Usuario)throws DAOException, DataExistsException;
 
 	void update(Usuario Usuario)throws DAOException;
 
