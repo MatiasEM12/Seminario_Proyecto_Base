238cef09860d0717d76c0aba96facad2fd16829d
i2hon <ian2honcharuk@gmail.com>
2026-01-29 03:17:58 -0300

    Update IApi.java
    agregue las excepciones faltantes

--- BACKEND/src/ar/edu/unrn/seminario/api/IApi.java
+++ BACKEND/src/ar/edu/unrn/seminario/api/IApi.java
@@ -15,7 +15,7 @@ public interface IApi {
 	  public void InicializarContadores() throws SQLException;
 	//Usuario
 
-	void registrarUsuario(String username, String password, String email, String nombre, Integer rol,boolean activo)  throws DataEmptyException, DataObjectException, DataNullException, DataDateException, SQLException, DAOException, DataExistsException ;
+	void registrarUsuario(String username, String password, String email, String nombre, Integer rol,boolean activo)  throws DataEmptyException, DataObjectException, DataNullException, DataDateException, SQLException, DAOException, DataExistsException, DataLengthException ;
 	
 	 
 	UsuarioDTO obtenerUsuario(String username) throws DAOException;
@@ -24,95 +24,95 @@ public interface IApi {
 	
 	List<UsuarioDTO> obtenerUsuarios() throws DataNullException, DAOException; // recuperar todos los usuarios
 
-	void activarUsuario(String username) throws StateChangeException ; // recuperar el objeto Usuario, implementar el comportamiento de estado.
+	void activarUsuario(String username) throws StateChangeException, DAOException ; // recuperar el objeto Usuario, implementar el comportamiento de estado.
 
-	void desactivarUsuario(String username)  throws StateChangeException; // recuperar el objeto Usuario, implementar el comportamiento de estado.
+	void desactivarUsuario(String username)  throws StateChangeException, DAOException; // recuperar el objeto Usuario, implementar el comportamiento de estado.
 	
 	public Boolean existeUsuario(String username ) throws DAOException;
 	
 	
 	//Rol
-	List<RolDTO> obtenerRoles() throws StateChangeException;
+	List<RolDTO> obtenerRoles() throws StateChangeException, DAOException, DataNullException;
 
 	List<RolDTO> obtenerRolesActivos() throws StateChangeException, DataNullException, DAOException;
 
-	void guardarRol(Integer codigo,String nombre, String descripcion, boolean estado) throws DataNullException; // crear el objeto de dominio  Rol
+	void guardarRol(Integer codigo,String nombre, String descripcion, boolean estado) throws DataNullException, DAOException; // crear el objeto de dominio  Rol
 
 
-	RolDTO obtenerRolPorCodigo(Integer codigo); // recuperar el rol almacenado
+	RolDTO obtenerRolPorCodigo(Integer codigo) throws DAOException; // recuperar el rol almacenado
 
-	void activarRol(Integer codigo) throws StateChangeException; // recuperar el objeto Rol, implementar el comportamiento de estado.
+	void activarRol(Integer codigo) throws StateChangeException, DAOException; // recuperar el objeto Rol, implementar el comportamiento de estado.
 
-	void desactivarRol(Integer codigo) throws StateChangeException; // recuperar el objeto Rol, imp
+	void desactivarRol(Integer codigo) throws StateChangeException, DAOException; // recuperar el objeto Rol, imp
 
 	
-	void guardarRol(RolDTO rol) throws DataNullException;
+	void guardarRol(RolDTO rol) throws DataNullException, DAOException;
 
 	
 	// API
-	void modificarContraseña(String usuario, String passWord) throws DataNullException;
+	void modificarContraseña(String usuario, String passWord) throws DataNullException, DAOException;
 	
-	public Boolean autenticar(String username, String password) throws DataNullException;
+	public Boolean autenticar(String username, String password) throws DataNullException, DAOException;
 	
 
 	
-	List<OrdenDTO> obtenerOrdenes();
+	List<OrdenDTO> obtenerOrdenes() throws DAOException;
 	
 	
 	//OrdenRetiro
 	
 	public void registrarOrdenRetiro(OrdenRetiroDTO retiro) throws DataNullException, DataLengthException, DataDoubleException, StateChangeException;
 
-	ArrayList<OrdenRetiroDTO> obtenerOrdenesRetiro();
+	ArrayList<OrdenRetiroDTO> obtenerOrdenesRetiro() throws DAOException;
 
-	public OrdenRetiroDTO obtenerOrdenRetiro(String codOrdenRetiro);
+	public OrdenRetiroDTO obtenerOrdenRetiro(String codOrdenRetiro) throws DAOException;
 	//public String obtenerUsernameVoluntarioPorOrdenRetiro(String codOrdenRetiro) ;
 	
 	//OrdenPedido 
 
-	ArrayList<OrdenPedidoDTO> obtenerOrdenesPedido();
+	ArrayList<OrdenPedidoDTO> obtenerOrdenesPedido() throws DAOException;
 
 	ArrayList<VisitaDTO> obtenerVisitas(String codOrdenRetiro) throws DataNullException, DataLengthException;
 	
 	//Donante
-	public void registrarDonante(Donante donante);
+	public void registrarDonante(Donante donante) throws DAOException;
 	List<DonanteDTO> obtenerDonantes(String userSolicitante);
-	List<UsuarioDTO> obtenerUserDonantes() throws DataNullException;
+	List<UsuarioDTO> obtenerUserDonantes() throws DataNullException, DAOException;
 
 	//Voluntario 
-	public void registrarVoluntario(Voluntario voluntario);
+	public void registrarVoluntario(Voluntario voluntario) throws DAOException;
 	List<VoluntarioDTO> obtenerVoluntarios();
-	List<UsuarioDTO> obtenerUserVoluntarios() throws DataNullException;
+	List<UsuarioDTO> obtenerUserVoluntarios() throws DataNullException, DAOException;
 	public String obtenerUsernameVoluntarioPorOrdenRetiro(String codOrdenRetiro);
 	//Administrador 
-	List<UsuarioDTO> obtenerUserAdministrador() throws DataNullException;
+	List<UsuarioDTO> obtenerUserAdministrador() throws DataNullException, DAOException;
 	
 	//Donacion
-	public void registrarDonacion(DonacionDTO donacion) throws DataNullException, DataDoubleException, DataEmptyException, DataObjectException, DataDateException;
-	ArrayList<DonacionDTO> obtenerDonaciones() throws DataNullException, DataEmptyException, DataObjectException, DataDateException;
+	public void registrarDonacion(DonacionDTO donacion) throws DataNullException, DataDoubleException, DataEmptyException, DataObjectException, DataDateException, DAOException;
+	ArrayList<DonacionDTO> obtenerDonaciones() throws DataNullException, DataEmptyException, DataObjectException, DataDateException, DAOException;
 	
 	
-	public ArrayList<DonacionDTO> obtenerDonacionesPendientes() throws DataNullException, DataEmptyException, DataObjectException, DataDateException;
+	public ArrayList<DonacionDTO> obtenerDonacionesPendientes() throws DataNullException, DataEmptyException, DataObjectException, DataDateException, DAOException;
 	
 	public DonacionDTO obtenerDonacion(String ordenP) throws DataNullException;
 	
-	public ArrayList<BienDTO> obtenerBienesDeVisita(String codVisita);
-	public BienDTO obtenerBien (String codigo);
-	public List<BienDTO> obtenerBienesPorOrdenRetiro(String codOrdenRetiro) ;
+	public ArrayList<BienDTO> obtenerBienesDeVisita(String codVisita) throws DataNullException, DAOException;
+	public BienDTO obtenerBien (String codigo) throws DataNullException, DAOException;
+	public List<BienDTO> obtenerBienesPorOrdenRetiro(String codOrdenRetiro) throws DAOException ;
 	
-	public void inicializarOrdenesRetiro(String codPedido) throws DataNullException;
+	public void inicializarOrdenesRetiro(String codPedido) throws DataNullException, DAOException;
 
-	ArrayList<BienDTO> obtenerBienesPorOrdenPedido(String codOP) throws DataNullException, DataEmptyException, DataObjectException, DataDateException;
+	ArrayList<BienDTO> obtenerBienesPorOrdenPedido(String codOP) throws DataNullException, DataEmptyException, DataObjectException, DataDateException, DAOException;
 
-	void guardarRol(Integer codigo, String descripcion, boolean estado) throws DataNullException;
+	void guardarRol(Integer codigo, String descripcion, boolean estado) throws DataNullException, DAOException;
 
-	void registrarUsuario(String username, String password, String email, String nombre, Integer codigoRol) throws DataEmptyException, SQLException, DAOException, DataExistsException;
+	void registrarUsuario(String username, String password, String email, String nombre, Integer codigoRol) throws DataEmptyException, SQLException, DAOException, DataExistsException, DataNullException, DataObjectException, DataLengthException;
 
 	//Visitas
-	public void registrarVisita(Visita visita);
-	public void cargarVisita(VisitaDTO visita) throws DataNullException, DataLengthException, DataDoubleException, StateChangeException;
-	 public void registrarOrdenPedido(OrdenPedido orden)throws DataNullException ;
-	 public void registrarOrdenPedido(OrdenPedidoDTO orden) throws DataNullException ;
+	public void registrarVisita(Visita visita) throws DAOException;
+	public void cargarVisita(VisitaDTO visita) throws DataNullException, DataLengthException, DataDoubleException, StateChangeException, DAOException;
+	public void registrarOrdenPedido(OrdenPedido orden)throws DataNullException, DAOException ;
+	public void registrarOrdenPedido(OrdenPedidoDTO orden) throws DataNullException ;
 
 	public String obtenerEstadoOrdenPedido(String codOrdenPedido) ;
 
@@ -125,13 +125,13 @@ public interface IApi {
 
 
 	void registrarOrdenRetiro1(OrdenRetiroDTO retiro)
-			throws DataNullException, DataLengthException, DataDoubleException, StateChangeException;
+			throws DataNullException, DataLengthException, DataDoubleException, StateChangeException, DAOException;
 
 	
-	public void registrarUbicacion(Ubicacion ubicacion);
+	public void registrarUbicacion(Ubicacion ubicacion) throws DAOException;
 
 
-	List<DonanteDTO> obtenerDonantes();
+	List<DonanteDTO> obtenerDonantes() throws DAOException;
 	 public  BienDTO toBienDTO(Bien bien);
 
 
@@ -139,15 +139,15 @@ public interface IApi {
 	 
 	// nuevo
 	// Inventario
-	public void eliminarBineInventario(String username);
+	public void eliminarBineInventario(String username) throws DataNullException, DAOException;
 	
 	
 	
-	public List<BienDTO> obtenerTodosLosBienes();
+	public List<BienDTO> obtenerTodosLosBienes() throws DAOException;
 	
-	public List<BienDTO> obtenerBienesPorTipo(String tipo);
+	public List<BienDTO> obtenerBienesPorTipo(String tipo) throws DataNullException, DAOException;
 	
-	public void ModificarBienInventario(Bien bien);
+	public void ModificarBienInventario(Bien bien) throws DAOException;
 	
 	public Bien ObtenerBien(String codigo) throws DataNullException, DAOException;
 	
