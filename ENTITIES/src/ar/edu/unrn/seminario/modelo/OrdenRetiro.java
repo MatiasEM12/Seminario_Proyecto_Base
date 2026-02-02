9269e121ea6fca6468c67ad0e134d2d7fb64cf51
MatiasEM12 <matiasemimellado@gmail.com>
2026-02-02 17:29:28 -0300

    OrdenRetiro excepciones

--- ENTITIES/src/ar/edu/unrn/seminario/modelo/OrdenRetiro.java
+++ ENTITIES/src/ar/edu/unrn/seminario/modelo/OrdenRetiro.java
@@ -22,13 +22,12 @@ public class OrdenRetiro extends Orden{
 	private ArrayList<Bien> recolectados;
 	
 	public OrdenRetiro( LocalDate fechaEmision, OrdenPedido pedido,
-			ArrayList<Visita> visitas)throws DataNullException{
+			ArrayList<Visita> visitas)throws DataNullException, DataObjectException, DataListException, DataDateException{
 		super(fechaEmision,EstadoOrden.PENDIENTE,tipo);
 		
-		if(pedido==null) {
-			throw new DataNullException("La orden de pedido no puede ser null");
-		}
-		
+		this.validarObjectNull(pedido);
+		this.validarListVisita(visitas);
+		this.validarDate(fechaEmision);
 		this.pedido = pedido;
 		this.visitas = visitas;
 		this.recolectados = new ArrayList<>();
@@ -39,14 +38,14 @@ public class OrdenRetiro extends Orden{
 
 
 	public OrdenRetiro( LocalDate fechaEmision, OrdenPedido pedido,
-			ArrayList<Visita> visitas,Voluntario voluntario) throws DataNullException{
+			ArrayList<Visita> visitas,Voluntario voluntario) throws DataNullException, DataObjectException, DataListException, DataDateException{
 		super(fechaEmision,EstadoOrden.PENDIENTE,tipo);
-		if(pedido==null) {
-			throw new DataNullException("La orden de pedido no puede ser null");
-		}
-		if(voluntario==null) {
-			throw new DataNullException("El voluntario no puede ser null");
-		}
+	
+		this.validarObjectNull(pedido);
+		this.validarListVisita(visitas);
+		this.validarObjectNull(voluntario);
+		this.validarDate(fechaEmision);
+		
 		this.pedido = pedido;
 		this.visitas = visitas;
 		this.recolectados = new ArrayList<>();
@@ -55,12 +54,15 @@ public class OrdenRetiro extends Orden{
 		}
 		
 
-		public OrdenRetiro( String codigo ,String estado,LocalDate fechaEmision, Voluntario voluntario, OrdenPedido ordenPedido,ArrayList <Visita> visitas){
+		public OrdenRetiro( String codigo ,String estado,LocalDate fechaEmision, Voluntario voluntario, OrdenPedido ordenPedido,ArrayList <Visita> visitas) throws DataObjectException, DataListException, DataDateException{
 			
 		
 			super(fechaEmision,estado,tipo);
 	
-	
+			this.validarObjectNull(pedido);
+			this.validarListVisita(visitas);
+			this.validarObjectNull(voluntario);
+			this.validarDate(fechaEmision);
 			this.pedido = ordenPedido;
 			this.visitas = visitas;
 			this.recolectados = new ArrayList<>();
@@ -77,30 +79,7 @@ public class OrdenRetiro extends Orden{
 	public String getEstadoRetiro() {
 		return super.getEstadoString();
 	}
-	/* 
-	public void setEstadoRetiro(String estadoRetiro) throws StateChangeException,DataNullException{
-	    if (estadoRetiro == null || estadoRetiro.isEmpty()) {
-	        throw new DataNullException("El campo 'estado de retiro' no puede estar vacío");
-	    }
-	    //si todas las condiciones son verdaderas entraria en el if
-	    if (!estadoRetiro.equals("Pendiente") && !estadoRetiro.equals("En proceso") && !estadoRetiro.equals("Completada") && !estadoRetiro.equals("Cancelada")) {
-	        throw new StateChangeException("El estado de retiro ingresado es invalido");
-	    }
-	   
-	    //actualisa la orden y el pedido si es necesario
-	    if(estadoRetiro.equals("Completada")  || estadoRetiro.equals("Cancelada")) {	    
-	    	if(estadoRetiro.equals("Completada")) {
-	    		super.setEstado(EstadoOrden.COMPLETADA);
-	    		
-	    	}
-	    	else {
-	    		super.setEstado(EstadoOrden.CANCELADA);
-	    		
-	    	}
-	    	
-	    }
-	} */
-	
+
 	
 	public void ordenEstadoCompleta() throws StateChangeException {
 		
@@ -141,14 +120,15 @@ public class OrdenRetiro extends Orden{
 	public OrdenPedido getPedido() {
 		return pedido;
 	}
-	public void setPedido(OrdenPedido pedido) {
+	public void setPedido(OrdenPedido pedido) throws DataObjectException {
+		this.validarObjectNull(pedido);
 		this.pedido = pedido;
 	}
 	public ArrayList<Visita> getVisitas() {
 		return visitas;
 	}
-	public void agregarVisita(Visita visita) throws StateChangeException {
-		
+	public void agregarVisita(Visita visita) throws StateChangeException, DataObjectException, DataListException {
+		this.validarObjectNull(visita);
 		
 		this.visitas.add(visita);
 		if(visita.getBienesRecolectados()!=null) {
@@ -162,11 +142,13 @@ public class OrdenRetiro extends Orden{
 		}
 		
 	}
-	public void agregarBien(Bien bien) {
+	public void agregarBien(Bien bien) throws DataObjectException {
+		this.validarObjectNull(bien);
 		this.recolectados.add(bien);
 	}
 	
-	public void agregarBienes (ArrayList<Bien> bienes) {
+	public void agregarBienes (ArrayList<Bien> bienes) throws DataListException {
+		this.validarListBien(bienes);
 		this.recolectados.addAll(bienes);
 	}
 	public String getCodigo() {
@@ -208,16 +190,19 @@ public class OrdenRetiro extends Orden{
 	}
 
 
-	public void setVoluntario(Voluntario voluntario) {
+	public void setVoluntario(Voluntario voluntario) throws DataObjectException {
+		this.validarObjectNull(voluntario);
 		this.voluntario = voluntario;
 	}
 
 
-	public void setRecolectados(ArrayList<Bien> recolectados) {
+	public void setRecolectados(ArrayList<Bien> recolectados) throws DataListException {
+		this.validarListBien(recolectados);
 		this.recolectados = recolectados;
 	}
 
-	public void setVisitas(ArrayList<Visita> visitas) {
+	public void setVisitas(ArrayList<Visita> visitas) throws DataListException {
+		this.validarListVisita(visitas);
 		this.visitas = visitas;
 	}
 
@@ -252,12 +237,23 @@ public class OrdenRetiro extends Orden{
 		}
 	}
 	
-	private void validarList( ArrayList<Bien> Bienes) throws DataListException {
-		if (Bienes==null) {
+	private void validarListVisita( ArrayList<Visita> visitas) throws DataListException {
+		if (visitas==null) {
 			throw new DataListException("List invalida");
+		}
+	}
+	private void validarDate(LocalDate fecha) throws DataDateException {
+		if (fecha==null) {
+			throw new DataDateException("La fecha no puede ser nula");
+		
 		}
 	}
 	
 
+	private void validarListBien( ArrayList<Bien> bienes) throws DataListException {
+		if (bienes==null) {
+			throw new DataListException("List invalida");
+		}
+	}
 
 }
