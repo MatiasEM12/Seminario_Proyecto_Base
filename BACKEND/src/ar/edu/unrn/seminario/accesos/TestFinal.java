b156f5f1d131b04f37aaa284aa750b256a05eb81
i2hon <ian2honcharuk@gmail.com>
2026-02-03 01:04:48 -0300

    Create TestFinal.java
    cree el test donde se inicializan los into que cargábamos manual mente en la base de datos.

---
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/TestFinal.java
@@ -0,0 +1,55 @@
+package ar.edu.unrn.seminario.accesos;
+
+import ar.edu.unrn.seminario.api.IApi;
+import ar.edu.unrn.seminario.api.PersistenceApi;
+import ar.edu.unrn.seminario.modelo.Rol;
+import ar.edu.unrn.seminario.modelo.Usuario;
+
+public class TestFinal {
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		try {
+			IApi api = new PersistenceApi();
+			
+			RolDao rolDAO = new RolDAOJDBC();
+			UsuarioDAOJDBC usuarioDAO = new UsuarioDAOJDBC();
+			//roles
+			//crea los roles base
+			Rol rol1 = new Rol(1,"Admin", true);
+			rolDAO.create(rol1);
+			
+			Rol rol2 = new Rol(2, "Voluntario", true);
+			rolDAO.create(rol2);
+			
+			Rol rol3 = new Rol(3, "Donante", true);
+			rolDAO.create(rol3);
+			
+			Rol rol4 = new Rol(4, "Beneficiario", true);
+			rolDAO.create(rol4);
+			
+			//usuarios
+			
+			Usuario.setContadorUsuario(usuarioDAO.obtenerCantidadUsuarios());
+			
+			// se puede cambiar en el costructor en ves de pasarle el tipo de rol que lo busque por codigo, ej: Rol rolx = rolDAO.find(1);
+			// o directamente en el costructor ("perry_AD","87654321","Jeff","perry_AD12@mail.com",rolDAO.find(1),true,null);
+			
+			Usuario usuario_1 = new Usuario("perry_AD","87654321","Jeff","perry_AD12@mail.com",rol1,true,null);
+			usuarioDAO.create(usuario_1);
+			
+			Usuario usuario_2 = new Usuario("pedro_Vol","12345678","Pedro.P","pedro_Vol12@mail.com",rol2,true,null);
+			usuarioDAO.create(usuario_2);
+			Usuario usuario_3 = new Usuario("ian_Don","12121212","Ian.H","ian_Don12@mail.com",rol3,true,null);
+			usuarioDAO.create(usuario_3);
+			
+			Usuario usuario_4 = new Usuario("matias_Ben","00010001","Matias.M","matias_Ben12@mail.com",rol4,true,null);
+			usuarioDAO.create(usuario_4);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
