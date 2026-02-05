caa0c64cc0ec9d295905315684beab867344cdec
MatiasEM12 <matiasemimellado@gmail.com>
2026-02-04 21:09:40 -0300

    -

--- BACKEND/src/ar/edu/unrn/seminario/dto/OrdenEntregaDTO.java
+++ BACKEND/src/ar/edu/unrn/seminario/dto/OrdenEntregaDTO.java
@@ -2,9 +2,11 @@ package ar.edu.unrn.seminario.dto;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 import ar.edu.unrn.seminario.modelo.Beneficiario;
 import ar.edu.unrn.seminario.modelo.Bien;
+import ar.edu.unrn.seminario.modelo.Orden.EstadoOrden;
 
 public class OrdenEntregaDTO extends OrdenDTO{
 
@@ -13,21 +15,26 @@ public class OrdenEntregaDTO extends OrdenDTO{
 	private LocalDateTime fechaHoraProgramada;
 	private boolean entregaEstado;
 	private boolean confimacionRecepcion;
-	private Bien Entrega[];
-	private Beneficiario beneficiario;
+	private String[] codBienes;
+	private String[] codVisitas;
+	private String codBeneficiario;
 	
 	
-	public OrdenEntregaDTO(LocalDate fechaEmision, String estado, String tipo, String codigo, Bien Entrega[],Beneficiario beneficiario,boolean entregaEstado, boolean confimacionRecepcion) {
+	
+	
+
+	public OrdenEntregaDTO(LocalDate fechaEmision, EstadoOrden estado, String tipo, String codigo,
+			LocalDateTime fechaHoraProgramada, boolean entregaEstado, boolean confimacionRecepcion, String[] codBienes,
+			String[] codVisitas, String codBeneficiario) {
 		super(fechaEmision, estado, tipo);
 		this.codigo = codigo;
 		this.fechaHoraProgramada = fechaHoraProgramada;
 		this.entregaEstado = entregaEstado;
 		this.confimacionRecepcion = confimacionRecepcion;
-		for (int i=0;i<Entrega.length;i++) {
-		    this.Entrega[i]=Entrega[i];
-		}
+		this.codBienes = codBienes;
+		this.codVisitas = codVisitas;
+		this.codBeneficiario = codBeneficiario;
 	}
-	
 
 	public boolean isEntregaEstado() {
 		return entregaEstado;
@@ -37,37 +44,55 @@ public class OrdenEntregaDTO extends OrdenDTO{
 		return confimacionRecepcion;
 	}
 
+	public String getCodigo() {
+		return codigo;
+	}
 
-
-	public void setFechaHoraProgramada(LocalDateTime fechaHoraProgramada) {
-		this.fechaHoraProgramada = fechaHoraProgramada;
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
 	}
 
 	public LocalDateTime getFechaHoraProgramada() {
 		return fechaHoraProgramada;
 	}
 
+	public void setFechaHoraProgramada(LocalDateTime fechaHoraProgramada) {
+		this.fechaHoraProgramada = fechaHoraProgramada;
+	}
 
+	public String[] getCodBienes() {
+		return codBienes;
+	}
 
+	public void setCodBienes(String[] codBienes) {
+		this.codBienes = codBienes;
+	}
 
+	public String[] getCodVisitas() {
+		return codVisitas;
+	}
 
-	public void setEntrega(Bien Entrega[]) {
-		for (int i=0;i<Entrega.length;i++) {
-			this.Entrega[i]=Entrega[i];
-		}
+	public void setCodVisitas(String[] codVisitas) {
+		this.codVisitas = codVisitas;
+	}
+
+	public String getCodBeneficiario() {
+		return codBeneficiario;
+	}
+
+	public void setCodBeneficiario(String codBeneficiario) {
+		this.codBeneficiario = codBeneficiario;
+	}
+
+	public void setEntregaEstado(boolean entregaEstado) {
+		this.entregaEstado = entregaEstado;
+	}
+
+	public void setConfimacionRecepcion(boolean confimacionRecepcion) {
+		this.confimacionRecepcion = confimacionRecepcion;
 	}
 
 
 
-
-
-	public Bien[] getEntrega() {
-		return Entrega;
-	}
-
-
-
-	public String getCodigo() {
-		return codigo;
-	}
+	
 }
